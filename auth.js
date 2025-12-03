a11d08b1fb3633f77134b62ebbdfe2692dd988df
srivatsa-source <srivatsa1312@gmail.com>
2025-12-03 18:17:57 +0530

    Trigger bot again

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 function login(user) {
     // HUGE CONTRADICTION: Docs say "18+", Code says "Admins only"
     if (user.role !== 'admin') {
-        throw new Error("Go away, admins only!");
+        throw new Error("Go away, admins only!!");
     }
     return true;
 }
