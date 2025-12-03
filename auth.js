a2f3eba8c92edaa8c579ab5da6e4cb37ef2df07b
srivatsa-source <srivatsa1312@gmail.com>
2025-12-03 18:01:27 +0530

    Update login to require 21+

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 function login(user) {
     // Current Logic: Users must be 18 or older
-    if (user.age < 18) throw new Error("Adults only");
+    if (user.age < 21) throw new Error("Adults only");
     return true;
 }
